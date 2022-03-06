a9d23d03eff411bbab2eca7b9ef0ae88cf65fbe2
Radu <radu@popescu.xyz>
2022-03-06 16:24:13 +0200

    Fixed tailwind

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: ``,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Freeze for peace`,
+    siteUrl: `https://freezeforpeace.org`,
   },
   plugins: [
     `gatsby-plugin-postcss`,
