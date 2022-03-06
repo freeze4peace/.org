467ab7d6f65e93ffdec8f822c28e5f353016eeaf
Radu <radu@popescu.xyz>
2022-03-06 19:39:50 +0200

    Added some hero widgets and footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,16 @@
 import * as React from "react";
-import Hero from "../components/hero";
+import Hero from "../components/hero3";
 import Navbar from "../components/navbar";
+import Cards from "../components/cards";
+import Footer from "../components/footer";
 
 export default function IndexPage() {
   return (
     <div className="relative bg-gray-50">
       <Navbar />
-
       <Hero />
+      <Cards />
+      <Footer />
     </div>
   );
 }
