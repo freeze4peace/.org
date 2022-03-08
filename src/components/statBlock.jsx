8c7b3ea1a1174ac068b545bd9d93cfe5e0b6ea73
Radu <radu@popescu.xyz>
2022-03-08 18:15:51 +0200

    Hero text

--- src/components/statBlock.jsx
+++ src/components/statBlock.jsx
@@ -3,7 +3,7 @@ import * as React from "react";
 export default function StatBlock({ statText, title, text, icon }) {
   return (
     <div className="flex flex-col items-center">
-      <div className="flex items-center justify-end h-24 px-6 text-3xl text-white w-60 button-bg recursive-font">
+      <div className="flex items-center justify-end h-24 px-6 text-3xl font-bold text-white w-60 button-bg recursive-font">
         <div className="custom-txt-shadow">{statText}</div>
       </div>
       <div className="w-64 px-4">
