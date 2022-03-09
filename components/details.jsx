5cc90a01ddbb13c9ca35fc6720ee061b95d5e970
Radu <radu@popescu.xyz>
2022-03-09 23:46:37 +0200

    Details widget

---
+++ components/details.jsx
@@ -0,0 +1,39 @@
+import Image from "next/image";
+import PutInImage from "../images/Put in.png";
+
+export default function Details() {
+  return (
+    <main className="mx-auto lg:flex lg:relative max-w-7xl recursive-font">
+      {/* <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
+      </div> */}
+      <div className="flex items-center justify-center w-full pt-16 pb-20 mx-auto lg:w-1/2">
+        <Image src={PutInImage} alt="PutIn" />
+      </div>
+      <div className="flex items-center w-full px-4 mx-auto lg:w-1/2">
+        <div>
+          <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl lg:text-5xl xl:text-6xl">
+            <div className="text-3xl ">Let’s hit Putin where it hurts!</div>{" "}
+            <div className="text-3xl text-blue-500 ">
+              right in the fossil fuels.
+            </div>
+          </h1>
+          <div className="pt-6 space-y-4">
+            <p>
+              Russias most important exports by more then 100B to the next
+              highest categories grains and timber are its mineral fuels: oil,
+              gas & coal.
+            </p>
+            <p>
+              They add up to more then 60% of the nations income and are
+              therefore a main driver of Putins war efforts.
+            </p>
+            <p>
+              Help us quickly spread the word to as many households as possible
+              and freeze for peace together.
+            </p>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+}
--- components/hero.jsx
+++ components/hero.jsx
@@ -1,9 +1,6 @@
 import Image from "next/image";
 import peaceImage from "../images/peace.png";
 
-// import "react-responsive-carousel/lib/styles/carousel.min.css";
-// import { Carousel } from "react-responsive-carousel";
-
 export default function Hero() {
   return (
     <main className="mx-auto lg:relative max-w-7xl recursive-font">
@@ -22,33 +19,7 @@ export default function Hero() {
         </div>
       </div>
       <div className="relative w-full h-64 sm:h-72 md:h-96 lg:absolute lg:inset-y-0 lg:right-0 lg:w-1/2 lg:h-full">
-        <Image
-          src={peaceImage}
-          alt="Peace"
-          className="inset-0 object-cover w-full h-full"
-          // width={500} automatically provided
-          // height={500} automatically provided
-          // blurDataURL="data:..." automatically provided
-          // placeholder="blur" // Optional blur-up while loading
-        />
-        {/* <StaticImage
-          src="../images/peace.png"
-          alt="Freeze for peace logo"
-          placeholder="blurred"
-          quality="100"
-          className="inset-0 object-cover w-full h-full"
-          backgroundColor="transparent"
-        /> */}
-        {/* <Carousel
-          infiniteLoop="true"
-          autoPlay={true}
-          showStatus={false}
-          showThumbs={false}
-        >
-          <img src="https://images.unsplash.com/photo-1520333789090-1afc82db536a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80" />
-          <img src="https://images.unsplash.com/photo-1640622307985-eaa37b36077f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" />
-          <img src="https://images.unsplash.com/photo-1580894732444-8ecded7900cd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2940&q=80" />
-        </Carousel> */}
+        <Image src={peaceImage} alt="Peace" placeholder={false} layout="fill" />
       </div>
     </main>
   );
