2fe29afe650573a545916de926f130cad2a66bef
Radu <radu@popescu.xyz>
2022-03-11 20:02:47 +0200

    Stat cards fix

--- components/cards.jsx
+++ components/cards.jsx
@@ -5,34 +5,36 @@ import bulletsIcon from "../images/bullets_icon.png";
 import conflictIcon from "../images/conflict_icon.png";
 
 export default function Cards() {
+  const dateWarStarted = new Date(1645671600 * 1000);
+  const Difference_In_Time = new Date().getTime() - dateWarStarted.getTime();
+  const daysFromStart = Math.floor(Difference_In_Time / (1000 * 3600 * 24));
+
   return (
     <div>
       <div className="pt-0 sm:pt-4">
         <div className="py-12 mt-10 bg-gray-50 sm:pb-16">
           <div className="relative">
             <div className="relative px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-              <div className="max-w-4xl mx-auto">
-                <dl className="md:grid md:grid-cols-3 md:gap-2">
-                  <StatBlock
-                    statText="~600B"
-                    title="Euro Daily Spending"
-                    icon={fuelIcon}
-                    text="each day by the West to Russia for fossil fuel imports"
-                  ></StatBlock>
-                  <StatBlock
-                    statText="~5-10"
-                    icon={bulletsIcon}
-                    title="Pieces of Ammunition"
-                    text="per hour are payed for by the average western household's heating"
-                  ></StatBlock>
-                  <StatBlock
-                    statText="13th"
-                    icon={conflictIcon}
-                    title="Day of the Invasion"
-                    text="since Putin decided to attack a sovereign state and start this war"
-                  ></StatBlock>
-                </dl>
-              </div>
+              <dl className="lg:grid lg:grid-cols-3 lg:gap-2">
+                <StatBlock
+                  statText="~600B"
+                  title="Euro Daily Spending"
+                  icon={fuelIcon}
+                  text="each day by the West to Russia for fossil fuel imports"
+                ></StatBlock>
+                <StatBlock
+                  statText="~5-10"
+                  icon={bulletsIcon}
+                  title="Pieces of Ammunition"
+                  text="per hour are payed for by the average western household's heating"
+                ></StatBlock>
+                <StatBlock
+                  statText={`${daysFromStart}th`}
+                  icon={conflictIcon}
+                  title="Day of the Invasion"
+                  text="since Putin decided to attack a sovereign state and start this war"
+                ></StatBlock>
+              </dl>
             </div>
           </div>
         </div>
--- components/statBlock.jsx
+++ components/statBlock.jsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 export default function StatBlock({ statText, icon, title, text }) {
   return (
     <div className="flex flex-col items-center recursive-font">
-      <div className="flex items-center justify-between h-24 px-6 text-3xl font-bold text-white w-60 button-bg">
-        <div className="w-12 h-full py-6">
+      <div className="flex items-center justify-between px-6 text-3xl font-bold text-white h-36 w-80 button-bg">
+        <div className="w-20 h-full">
           <Image src={icon} alt={statText} />
         </div>
-        <div className="custom-txt-shadow">{statText}</div>
+        <div className="text-5xl custom-txt-shadow">{statText}</div>
       </div>
       <div className="w-64 px-4">
         <div className="w-full text-xl font-bold text-blue-500">{title}</div>
