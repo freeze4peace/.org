7f1eaddbf3127f2ff022d3a0d057d018d4e2ae3d
jaronschulz <jaronschulz@gmail.com>
2022-03-25 20:29:51 +0100

    better footer + imprint page & link

--- components/footer.jsx
+++ components/footer.jsx
@@ -1,27 +1,26 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import zero from "../images/zero.svg";
+import cc from "../images/cc.svg";
+
 export default function Footer() {
   return (
     <footer
-      className="px-12 mx-auto py-14 lg:py-0 lg:flex lg:relative max-w-7xl recursive-font"
+      className="px-12 my-12 mx-auto py-14 lg:py-0 lg:flex lg:relative max-w-7xl justify-between recursive-font items-center"
       aria-labelledby="footer-heading"
     >
+      <div className='lg:flex'>
       <p
-        xmlnsCc="http://creativecommons.org/ns#"
-        xmlnsDct="http://purl.org/dc/terms/"
+      className="mr-12"
+      xmlnsCc="http://creativecommons.org/ns#"
+      xmlnsDct="http://purl.org/dc/terms/"
       >
         <a
           property="dct:title"
           rel="cc:attributionURL"
           href="https://www.freezeforpeace.org"
-        >
+          >
           Freeze for Peace
-        </a>{" "}
-        by{" "}
-        <a
-          rel="cc:attributionURL dct:creator"
-          property="cc:attributionName"
-          href="https://www.linkedin.com/in/jaronschulz"
-        >
-          Jaron Schulz{" "}
         </a>{" "}
         is marked with{" "}
         <a
@@ -29,26 +28,26 @@ export default function Footer() {
           target="_blank"
           rel="license noopener noreferrer"
           style={{ display: "inline-block " }}
-        >
+          >
           CC0 1.0
-          <img
-            style={{
-              height: "22px!important",
-              marginLeft: "3px",
-              verticalAlign: "text-bottom",
-            }}
-            src="https://mirrors.creativecommons.org/presskit/icons/cc.svg?ref=chooser-v1"
-          />
-          <img
-            style={{
-              height: "22px!important",
-              marginLeft: "3px",
-              verticalAlign: "text-bottom",
-            }}
-            src="https://mirrors.creativecommons.org/presskit/icons/zero.svg?ref=chooser-v1"
-          />
-        </a>
-      </p>
+          </a>
+        </p>
+          <div className="flex justify-start">
+            <Image
+                height={25}
+                alt="Creative Commons Logo"
+                src={cc}
+                />
+            <Image
+              height={25}
+              alt="Creative Commons License Level"
+              src={zero}
+              />
+          </div>
+        </div>
+        <Link href="/imprint">
+          <a>Legal Disclosure</a>
+        </Link>
     </footer>
   );
 }
